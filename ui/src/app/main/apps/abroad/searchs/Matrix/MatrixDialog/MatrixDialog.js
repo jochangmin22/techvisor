14221009490321fa1b7e2ed19ac44abc6dbf2423
Kim Joongwan <hithats@gmail.com>
2020-10-26 00:43:39 +0000

    Minor Fix

--- ui/src/app/main/apps/abroad/searchs/Matrix/MatrixDialog/MatrixDialog.js
+++ ui/src/app/main/apps/abroad/searchs/Matrix/MatrixDialog/MatrixDialog.js
@@ -3,6 +3,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useCallback } from 'react';
 import { useEffect } from 'react';
 import { closeMatrixDialog } from 'app/main/apps/search/store/searchsSlice';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Typography from '@material-ui/core/Typography';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -11,8 +14,13 @@ import MatrixDialogTable from '../MatrixDialogTable';
 import Icon from '@material-ui/core/Icon';
 import IconButton from '@material-ui/core/IconButton';
 
+const useStyles = makeStyles(theme => ({
+	root: { backgroundColor: theme.palette.primary.dark }
+}));
+
 function MatrixDialog() {
 	const dispatch = useDispatch();
+	const classes = useStyles();
 	const matrixDialog = useSelector(({ abroadApp }) => abroadApp.searchs.matrixDialog);
 
 	const initDialog = useCallback(() => {
@@ -37,7 +45,11 @@ function MatrixDialog() {
 	return (
 		<Dialog classes={{ paper: 'm-24' }} {...matrixDialog.props} onClose={closeDialog} fullWidth maxWidth="lg">
 			<DialogActions className="justify-between p-8 h-52">
-				<DialogTitle>검색 결과 ({Number(matrixDialog.data.length).toLocaleString()})</DialogTitle>
+				<DialogTitle>
+					<Typography className={clsx(classes.root, 'text-13 font-400 rounded-4 text-white px-8 py-4 mr-8')}>
+						검색 결과 {Number(matrixDialog.data.length).toLocaleString()} 건
+					</Typography>
+				</DialogTitle>
 				<IconButton onClick={closeDialog}>
 					<Icon>close</Icon>
 				</IconButton>
