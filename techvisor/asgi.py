eefc87d9558d226b17db3d717a7a75af8b8f6918
root <root@DESKTOP-N4O1EMO.localdomain>
2020-11-25 14:27:37 +0900

    Initial

--- techvisor/asgi.py
+++ techvisor/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ipgrim project.
+ASGI config for techvisor project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ipgrim.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'techvisor.settings')
 
 application = get_asgi_application()
--- techvisor/celery.py
+++ techvisor/celery.py
@@ -3,9 +3,9 @@ import os
 from celery import Celery
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ipgrim.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'techvisor.settings')
 
-app = Celery('ipgrim', backend='redis://', broker='redis://localhost:6379/0')
+app = Celery('techvisor', backend='redis://', broker='redis://localhost:6379/0')
 app.config_from_object('django.conf:settings', namespace='CELERY')  
 
 app.autodiscover_tasks()  
